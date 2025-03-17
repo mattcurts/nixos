1979e1fd189af68e65ce76108ccb16803c184f36
Matthew Curtis <Matthewcurtis@uvic.ca>
2025-03-17 01:11:10 -0700

    fix: prevent crashes

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -99,6 +99,8 @@
     };
   };
 
+  services.fwupd.enable = true;
+
   # Enable the KDE Plasma Desktop Environment.
   services.displayManager.sddm.enable = true;
   services.desktopManager.plasma6.enable = true;
