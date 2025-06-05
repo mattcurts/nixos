95bb2138e22d64e47f4d87e991e64c7cc380ab44
mattcurts <63571383+mattcurts@users.noreply.github.com>
2025-06-04 21:09:33 -0700

    Update startup.sh
    fix typo

--- startup.sh
+++ startup.sh
@@ -4,7 +4,7 @@
 
 git clone https://github.com/mattcurts/nixos.git
 cd nixos
-ix --version
+nix --version
 export NIX_CONFIG="experimental-features = nix-command flakes"
 sudo nixos-rebuild switch --flake .#nixos
 sudo nix-collect-garbage -d
